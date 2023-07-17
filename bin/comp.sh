fae1131820c35818ad1512197877f766a53c1c6e
Nova-archx86 <moorcode66@yahoo.com>
2023-07-17 00:20:23 -0700

    Refactored shell scripts

--- bin/comp.sh
+++ bin/comp.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# simple shell script that i made soley for 
+# the purpose of lazily verifying two checksum/hashes
+
 if [[ $1 == $2 ]] 
 then
 	echo "Match"
--- bin/install.sh
+++ bin/install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 get_repos() {
   git clone https://github.com/Nova-archx86/Dwm-build
   git clone https://github.com/Nova-archx86/slock-build 
@@ -11,10 +12,57 @@ build_paru() {
   cd ..
 }
 
+install_display_server() {
+	echo -e "What display server should be installed?\n1.\tX11(Legacy)\n2.\tWayland\n3.\tNone\n"
+	read CHOICE;
+
+	case $CHOICE in
+		"1")
+			paru -S xorg xorg-xinit
+			;;
+
+		"2")
+			paru -S wayland hyprland
+			;;
+
+		"3")
+			echo -e "Skipping display server install...\n"
+			;;
+
+		*)
+			echo -e "Error not a valid option!\n"
+			;;
+
+		esac
+
+}
+
 build_suckless() {
-	cd Dwm-build && make && sudo make install
-  cd ../slock-build && make && sudo make install
-  cd ..
+	read CHOICE -p "Would you like to use suckless utils (Requires X11)"
+
+	case $CHOICE in 
+		"y")
+			if [[ -d /usr/include/X11 ]] # chek for X11 libs
+			then
+				cd Dwm-build
+				make && sudo make install
+				cd ../slock-build
+				make && sudo make install
+				cd ..
+			else
+				echo -e "Error X11 libs are not installed!\n"
+			fi
+			;;
+
+	"n")
+		echo -e "Skipping suckless builds...\n"
+		;;
+
+	*)
+		echo -e "Error not an a valid option!\n"
+		;;
+
+	esac
 }
 
 install_packages() {
@@ -30,7 +78,7 @@ build_paru() {
 
 get_dotfiles() {
   cp -r .config $HOME 
-  cp -r wallpapers $HOME
+  cp -r Wallpapers $HOME
   cp -r bin $HOME
   cp .bash_profile $HOME
   cp .bashrc $HOME
@@ -40,6 +88,7 @@ get_dotfiles() {
 
 get_repos
 build_paru
-install_packages
+install_display_server
 build_suckless
+install_packages
 get_dotfiles
