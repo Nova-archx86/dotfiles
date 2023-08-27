e8cc8cba92941d2432b13b5880e61d5078af88e9
Nova-archx86 <moorcode66@yahoo.com>
2023-08-26 23:09:55 -0700

    added a wiki search script for browsing the arch wiki offline

---
+++ bin/wiki-seach.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Script idea courtousey of distro-tube
+dir="/usr/share/doc/arch-wiki/html/en"
+browser=librewolf # not using xdg-open due to having multiple browsers installed
+docs=$(find ${dir} -iname "*.html")
+
+choice=$(printf '%s\n' "${docs[@]}" | \
+	cut -d '/' -f8- | \
+	dmenu -i -l 20 -p "arch-wiki docs: ")
+
+if [ "$choice" ]; then
+	$browser "$dir/$choice"
+else
+	exit 0
+fi
