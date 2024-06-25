6f16a5da7571f79280656c854b26fd238dec8a0d
David Moor <moorcode66@yahoo.com>
2024-06-25 14:43:33 -0700

    updated music script to handle empty strings from dmenu

--- bin/music.sh
+++ bin/music.sh
@@ -2,6 +2,13 @@
 
 dir=/mnt/hdd
 font="Mononoki Nerd Font:size=15"
-opts="-sb #00FF00 -l 10"
-choice=$(ls $dir/music | dmenu -fn "$font" $opts)
-mpv --shuffle $dir/music/$choice
+opts="-nb #222222 -sb #ADFF2F -l 10"
+choice="$(ls $dir/music | dmenu -fn "$font" $opts)"
+
+if [ -z "$choice" ];
+then
+	exit 0
+else
+	mpv --shuffle $dir/music/$choice
+fi
+
