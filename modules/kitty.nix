d917faac70a77054fc6cca2a2e46137db075cd1d
Nova-archx86 <moorcode66@yahoo.com>
2025-10-23 17:49:00 -0700

    added kitty config

---
+++ modules/kitty.nix
@@ -0,0 +1,17 @@
+{ config, lib, pkgs, ... }:
+
+{
+  programs.kitty = {
+    enable = true;
+    shellIntegration.enableZshIntegration = true;
+    themeFile = "current-theme.conf"; # Usually set by kitty +kitten themes
+    font.package = pkgs.nerd-fonts._0xproto;
+    enableGitIntegration = true;
+
+    settings = {
+      background_opacity = 0.9;
+
+    };
+    
+  };
+}
