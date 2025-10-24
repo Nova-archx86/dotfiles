f024fd9826cc010705c153baa348253c7e081cd6
Nova-archx86 <moorcode66@yahoo.com>
2025-10-24 12:00:37 +0000

    added doom binaries to PATH

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -8,6 +8,7 @@
     enableCompletion = true;
     initContent = ''
       export PROMPT="%F{cyan} %3~%f"$'\n'" λ "
+      export PATH=/home/nova/.config/emacs/bin/:$PATH
     '';
     shellAliases = {
       nrs = "sudo nixos-rebuild switch --impure --flake /home/nova/dotfiles/";
